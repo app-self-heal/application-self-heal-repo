d04cf855822c3130c934a02ea0b74f17b850d3cc
app-self-heal <67695368+app-self-heal@users.noreply.github.com>
2020-07-01 19:03:40 +0530

    Add files via upload

---
+++ Application_SelfHealing/utils/checkRuleset.py
@@ -0,0 +1,25 @@
+import pandas as pd
+
+class check_rule_set:
+
+    def __init__(self, job_name, code):
+        self.job_name = job_name
+        self.code = code
+
+    def check_ruleset(self):
+        rule_file_path = r"C:\Users\noejoseph\Documents\PythonProjects\RuleSet.csv"
+        pdata = pd.read_csv(rule_file_path)
+        pdata = pdata[(pdata['JobName'] == self.job_name) & (pdata['SelfHeal_Flag'] == 'Yes') & (pdata['ErrorCode'] == self.code)]
+
+        pdata = pdata[['SelfHeal_Steps']]
+        if pdata.empty :
+            print('No rules specifies for this ruleset')
+        else:
+            self_heal_step = pdata.iat[0,0]
+
+            if self_heal_step == 'Re-run Job':
+                print('re-run job')
+
+            elif self_heal_step == 'Email Notification':
+                print('Email Notification')
+                #Invoke the email notification functionality here
