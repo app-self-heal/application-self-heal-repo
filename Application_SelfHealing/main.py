f4671a908078d65fb39f26386f464935e7e33c67
app-self-heal <67695368+app-self-heal@users.noreply.github.com>
2020-07-13 10:03:10 +0530

    Add files via upload

--- Application_SelfHealing/main.py
+++ Application_SelfHealing/main.py
@@ -1,6 +1,7 @@
 import csv
 import datetime
 import os
+import glob
 import utils.logFileParser as parser
 import utils.checkRuleset as cr
 import utils.self_heal_report as logger
@@ -10,23 +11,34 @@ config = param()
 
 if __name__ == "__main__":
     log_directory = config['log_file_path']
-    for filename in os.listdir(log_directory):
+    log_archive = config['log_archive']
+    polling_flag = 1
+    while polling_flag == 1:
+        # Check for termination file
+        termination_file_count = len(glob.glob1(log_directory,"*.termination"))
+        logfile_count = len(glob.glob1(log_directory,"*.log"))
 
-        """
-        If the logfile needs to be renamed to a .done file post processing, the below logic can be implemented.
-        So that it will not be re-processed on the next run-
-        
-         os.rename(os.path.join(dir,filename), os.path.join(dir,filename+'.done'))
-        """
-        #Instantiating the parser object for informatica log files
-        obj_parser = parser.log_file_parser(filename)
-        val = obj_parser.log_file_parser_informatica()
-        print(val)
+        if (termination_file_count > 0 & logfile_count == 0):
+            polling_flag = 0
 
-        #Instantiating the rule check object to trigger the corrective action.
-        obj_crs = cr.check_rule_set(val['job'], val['code'])
-        ruleset_return = obj_crs.check_ruleset()
+        for filename in os.listdir(log_directory):
 
-        #Creating log
-        SelfHeal_Start_Date_Time = datetime.datetime.now().strftime(" %B %d %H:%M:%S %Y") if ruleset_return['rule_flag'] == 'true' else 'NA'
-        logger.self_heal_report(ruleset_return['platform'],  val['job'], val['code'], val['error_time'], ruleset_return['action_taken'], SelfHeal_Start_Date_Time)
+            if filename.endswith(".log"):
+                #Instantiating the parser object for informatica log files
+                obj_parser = parser.log_file_parser(filename)
+                val = obj_parser.log_file_parser_informatica()
+                print(val)
+
+                #Instantiating the rule check object to trigger the corrective action.
+                obj_crs = cr.check_rule_set(val['job'], val['code'])
+                ruleset_return = obj_crs.check_ruleset()
+
+                #Creating log
+                SelfHeal_Start_Date_Time = datetime.datetime.now().strftime(" %B %d %H:%M:%S %Y") if ruleset_return['rule_flag'] == 'true' else 'NA'
+                logger.self_heal_report(ruleset_return['platform'],  val['job'], val['code'], val['error_time'], ruleset_return['action_taken'], SelfHeal_Start_Date_Time)
+
+                #Move the processed log-file to archive folder
+                os.rename(os.path.join(log_directory,filename), os.path.join(log_archive,filename))
+
+            elif filename.endswith(".termination"):
+                os.remove(os.path.join(log_directory,filename))
--- Application_SelfHealing/utils/config/params.py
+++ Application_SelfHealing/utils/config/params.py
@@ -3,6 +3,7 @@ def config():
           "log_file_path": r"C:\Users\noejoseph\Documents\PythonProjects\Application_SelfHealing\Logs",
           "rule_set_file": r"C:\Users\noejoseph\Documents\PythonProjects\RuleSet.csv",
           "self_heal_report": r"C:\Users\noejoseph\Documents\PythonProjects\Application_SelfHealing\SelfHealLogs.csv",
+          "log_archive": r"C:\Users\noejoseph\Documents\PythonProjects\Application_SelfHealing\Archive",
           "recepient_enail": "noejoseph@deloitte.com"
         }
 
